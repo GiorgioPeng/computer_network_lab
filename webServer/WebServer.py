904ed3e9d7c9c1fd4c3e65136cef9a37b0a39e7a
Giorgio <GiorgioPeng@github.com>
2019-10-20 20:39:55 +0800

    finish
    完成了大多数要求的功能

--- webServer/WebServer.py
+++ webServer/WebServer.py
@@ -7,22 +7,34 @@ import sys
 
 def handleRequest(tcpSocket):
 	# 1. Receive request message from the client on connection socket
-	conn, address = tcpSocket.accept()
-	# 2. Extract the path of the requested object from the message (second part of the HTTP header)
-	data = conn.recv(1024)
-	data = data.decode().split('\n')
-	filePath = data[0].split(' ')[1][1:]
+	while True:
+		conn, address = tcpSocket.accept()
+		# 2. Extract the path of the requested object from the message (second part of the HTTP header)
+		data = conn.recv(1024)
+		print(data.decode())
+		data = data.decode().split('\n')
+		try:
+			filePath = data[0].split(' ')[1][1:]
+		except IndexError as e:
+			pass
 
-	# 3. Read the corresponding file from disk
-	f = open(filePath,'r')
-	# 4. Store in temporary buffer
-	buffer = f.read()
-	# 5. Send the correct HTTP response error
-	print(buffer)
-	# 6. Send the content of the file to the socket
-	conn.sendto(buffer.encode(),address)
-	# 7. Close the connection socket
-	conn.close()
+		try:
+			# 3. Read the corresponding file from disk
+			f = open(filePath,'rb')
+			# 4. Store in temporary buffer
+			buffer = f.read()
+			# 5. Send the correct HTTP response error
+			conn.sendall(bytes('HTTP/1.1 200 OK\r\n\r\n', 'utf8'))
+			# 6. Send the content of the file to the socket
+			conn.sendall(buffer)
+			# 7. Close the connection socket
+		except FileNotFoundError as fe:
+			f = open('404.html','rb')
+			buffer = f.read()
+			conn.sendall(bytes('HTTP/1.1 404 Not Found\r\n\r\n','utf8'))
+			conn.sendall(buffer)
+
+		conn.close()
 	pass # Remove/replace when function is complete
 
 def startServer(serverAddress, serverPort):
@@ -31,9 +43,9 @@ def startServer(serverAddress, serverPort):
 	# 2. Bind the server socket to server address and server port
 	server.bind((serverAddress,serverPort))
 	# 3. Continuously listen for connections to server socket
-	server.listen(5)
+	server.listen(1)
 	# 4. When a connection is accepted, call handleRequest function, passing new connection socket (see https://docs.python.org/2/library/socket.html#socket.socket.accept)
-	handleRequest(server);
+	handleRequest(server)
 	# 5. Close server socket
 	server.close()
 	pass # Remove/replace when function is complete
